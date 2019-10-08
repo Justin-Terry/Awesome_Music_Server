fafb6d122f9939c508b519f53cb90dfb65ef6cc1
Justin Terry <Justin.Terry@student.csulb.edu>
2019-10-08 09:08:40 -0700

    Cleaned up examples

--- AwesomeMusicServer/src/tests/UDPTest.java
+++ AwesomeMusicServer/src/tests/UDPTest.java
@@ -21,15 +21,17 @@ public class UDPTest {
 //            response = client.sendRequest("{\"remoteMethod\":\"signInUser\"," +
 //                                         " \"objectName\": \"LoginServices\"," +
 //                                         " \"param\" : {\"email\": \"foo@email.com\", " +
-//                                         			   "\"password\" : \"password\"}" +
+//                                         			     "\"password\" : \"password\"}" +
 //                                         "}");
+        	
             // signUpUser Example
 //        	response = client.sendRequest("{\"remoteMethod\":\"z\"," +
-//                    			    "\"objectName\": \"LoginServices\"," +
-//                    			    "\"param\" : {\"name\" : \"Justin Terry\"," +
-//                    			    			 "\"email\" : \"foobbar@email.com\", " +
-//                    			    			 "\"password\" : \"password\"}" +
-//                    				"}");
+//                    			    	   "\"objectName\": \"LoginServices\"," +
+//                    			    	   "\"param\" : {\"name\" : \"Justin Terry\"," +
+//                    			    			 		"\"email\" : \"foobbar@email.com\", " +
+//                    			    			 		"\"password\" : \"password\"}" +
+//                    					   "}");
+        	
         	// searchForSong Example
         	// If page is empty, the return will be an empty array i.e. {"ret": "[]"}
 //        	response = client.sendRequest("{\"remoteMethod\":\"searchForSong\"," +
@@ -38,29 +40,33 @@ public class UDPTest {
 //						    			   			    "\"pageNumber\" : \"1\"} " +
 //						    			  "}");
         	
+        	// Create a new Playlist
 //        	response = client.sendRequest("{\"remoteMethod\":\"createPlaylist\"," +
-//	    			   "\"objectName\": \"PlaylistServices\"," +
-//	    			   "\"param\" : {\"playlistname\" : \"Test Playlist\"," +
-//		   			   				"\"userId\" : \"1\"} " +
-//        			   "}");
+//	    			   					   "\"objectName\": \"PlaylistServices\"," +
+//	    			   					   "\"param\" : {\"playlistname\" : \"Test Playlist\"," +
+//		   			   									"\"userId\" : \"1\"} " +
+//        			   					   "}");
         	
+        	// Add a song to a playlist
 //        	response = client.sendRequest("{\"remoteMethod\":\"addSongToPlaylist\"," +
-//	    			   "\"objectName\": \"PlaylistServices\"," +
-//	    			   "\"param\" : {\"userId\" : \"1\"," +
-//		   			   				"\"playlistName\" : \"Test Playlist\"," +
-//		   			   				"\"songId\" : \"SOEIJSK12AB0183835\"} " +
-//     			   "}");
+//	    			   					   "\"objectName\": \"PlaylistServices\"," +
+//	    			   					   "\"param\" : {\"userId\" : \"1\"," +
+//		   			   									"\"playlistName\" : \"Test Playlist\"," +
+//		   			   									"\"songId\" : \"SOEIJSK12AB0183835\"} " +
+//     			  						   "}");
         	
+        	// Get all playlists for a specific userId
 //        	response = client.sendRequest("{\"remoteMethod\":\"getPlaylists\"," +
-//			   "\"objectName\": \"PlaylistServices\"," +
-//			   "\"param\" : {\"userId\" : " + 1 + "} " +
-//			  "}");
+//			   							   "\"objectName\": \"PlaylistServices\"," +
+//			   							   "\"param\" : {\"userId\" : \"1\"} " +
+//			  							  "}");
         	
+        	// Delete a playlist
         	response = client.sendRequest("{\"remoteMethod\":\"removePlaylist\"," +
-			   "\"objectName\": \"PlaylistServices\"," +
-			   "\"param\" : {\"playlistname\" : \"Best Songs!\"," +
-			   				"\"userId\" : \"1\"} " +
-			   "}");
+									   	   "\"objectName\": \"PlaylistServices\"," +
+									       "\"param\" : {\"playlistname\" : \"Best Songs!\"," +
+									   					"\"userId\" : \"1\"} " +
+									       "}");
         	
         	System.out.println(response);
         } catch (IOException e1) {
